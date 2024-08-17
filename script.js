5f3c73825417f2359982e41a8581df7c118b4e81
Iryna Pih <irynapih@gmail.com>
2024-08-16 23:22:40 -0400

    update

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const userInpSection = document.getElementById("user-input-section");
 const resultText = document.getElementById("result");
 const word = document.getElementById("word");
 const tryAgainBtn = document.getElementById("tryAgain");
+const gameOverSound = document.getElementById("gameOverSound");
 
 const words = Object.keys(options);
 let randomWord = "",
@@ -25,3 +26,14 @@ let winCount = 0,
   lossCount = 0;
 
 const generateRandomValue = (array) => Math.floor(Math.random() * array.length);
+const blocker = () => {
+  let lettersButtons = document.querySelectorAll(".letters");
+  lettersButtons.forEach((button) => (button.disabled = true));
+  stopGame();
+};
+
+const stopGame = () => {
+  controls.classList.remove("hide");
+  tryAgainBtn.classList.remove("hide"); // Show the "Try Again" button
+  startBtn.classList.add("hide"); // Hide the "Start" button
+};
